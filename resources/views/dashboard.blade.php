dbb76ea964fef26f0a15c142b2e5198b78ceb389
Janith K. Gunathilake <janithkavinda49@gmail.com>
2024-09-20 17:54:07 +0530

    dashboard px issue

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -56,7 +56,7 @@
     </style>
 </head>
 <body class="flex items-center justify-center min-h-screen">
-    <div class="absolute top-0 bottom-0 bg-black bg-opacity-30 text-white w-full sm:w-[24rem] md:w-[32rem] lg:w-[40rem] h-[750] rounded-lg overflow-hidden shadow-lg">
+    <div class="absolute top-0 bottom-0 bg-black bg-opacity-30 text-white w-full sm:w-[24rem] md:w-[32rem] lg:w-[40rem] h-[750px] rounded-lg overflow-hidden shadow-lg">
         <div class="flex justify-between items-center bg-black py-4 px-6">
             <div class="text-white font-bold text-lg md:text-2xl"><img src="img/logo w.gif" alt="Logo" class="w-40 h-auto"></div>
             <!-- User Name and Logout -->
